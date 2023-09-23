1e33a7a67cbfde3c7de7de40db74b9d5c235ad50
jeffrate-productions <145793966+jeffrate-productions@users.noreply.github.com>
2023-09-22 22:23:54 -0400

    Delete src directory

--- src/shared/constants.js
+++
@@ -1,7 +0,0 @@
-export const APP_NAME="Island Flavor";
-export const ADDRESS_FIRST_LINE="555 Street Address";
-export const ADDRESS_SECOND_LINE="City, State, 10101";
-export const HERO_OVERLAY_MESSAGE="Delicious Food at Affordable Prices!"
-export const COPYRIGHT="© 2022 ISLAND FLAVOR";
-export const CUSTOMER_SERVICE_AIM="We aim to offer you high quality delicious Caribbean Cuisine in a clean environment that makes you feel at home."
-export const PLACE_ORDER_LINK="https://www.doordash.com/store/c-&-e-island-flavor,-llc-orangeburg-766649/?pickup=true&utm_campaign=gpa"
